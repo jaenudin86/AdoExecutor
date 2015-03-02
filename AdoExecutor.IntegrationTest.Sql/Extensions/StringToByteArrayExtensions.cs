ee6cbcf690638ba1c9f0385781e8543403c74ba6
treformat <t.reformat@hotmail.com>
2015-03-02 21:08:45 +0100

    add integration test to dynamic result type

---
+++ AdoExecutor.IntegrationTest.Sql/Extensions/StringToByteArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdoExecutor.IntegrationTest.Sql.Extensions
+{
+  public static class StringToByteArrayExtensions
+  {
+    public static byte[] ToByteArray(this string hex)
+    {
+      var numberChars = hex.Length;
+      var bytes = new byte[numberChars/2];
+      for (var i = 0; i < numberChars; i += 2)
+        bytes[i/2] = Convert.ToByte(hex.Substring(i, 2), 16);
+
+      return bytes;
+    }
+  }
+}
