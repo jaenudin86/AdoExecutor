a1c6c1de9c135027d05b48efdb3c3fc9587f8d45
treformat <t.reformat@hotmail.com>
2015-04-15 19:24:20 +0200

    Add base class for query factory

---
+++ AdoExecutor/Core/QueryFactory/QueryFactoryBase.cs
@@ -0,0 +1,61 @@
+﻿using AdoExecutor.Core.Configuration.Infrastructure;
+using AdoExecutor.Core.ObjectBuilder;
+using AdoExecutor.Core.ParameterExtractor;
+using AdoExecutor.Core.Query.Infrastructure;
+using AdoExecutor.Core.QueryFactory.Infrastructure;
+using AdoExecutor.Utilities.Adapter.DataTable;
+using AdoExecutor.Utilities.Adapter.List;
+using AdoExecutor.Utilities.ObjectConverter;
+using AdoExecutor.Utilities.PrimitiveTypes;
+
+namespace AdoExecutor.Core.QueryFactory
+{
+  public abstract class QueryFactoryBase : IQueryFactory
+  {
+    public virtual IQuery CreateQuery()
+    {
+      var configuration = CreateConfiguration();
+      return new Query.Query(configuration);
+    }
+
+    protected virtual IConfiguration CreateConfiguration()
+    {
+      var configuration = new Configuration.Configuration();
+
+      ConfigureConnectionStringProvider(configuration);
+      ConfigureDataObjectFactory(configuration);
+      ConfigureObjectBuilders(configuration);
+      ConfigureInterceptors(configuration);
+      ConfigureParameterExtractors(configuration);
+
+      return configuration;
+    }
+
+    protected abstract void ConfigureConnectionStringProvider(Configuration.Configuration configuration);
+    protected abstract void ConfigureDataObjectFactory(Configuration.Configuration configuration);
+
+    protected virtual void ConfigureObjectBuilders(Configuration.Configuration configuration)
+    {
+      configuration.ObjectBuilders.Add(new DataSetObjectBuilder(new DataTableAdapter()));
+      configuration.ObjectBuilders.Add(new DataTableObjectBuilder(new DataTableAdapter()));
+      configuration.ObjectBuilders.Add(new SimpleTypeObjectBuilder(new SqlPrimitiveDataTypes(), new ListAdapterFactory(),
+        new ObjectConverter()));
+      configuration.ObjectBuilders.Add(new DefinedTypeObjectBuilder(new ListAdapterFactory(),
+        new SqlPrimitiveDataTypes(), new ObjectConverter()));
+      configuration.ObjectBuilders.Add(new DynamicObjectBuilder(new ListAdapterFactory()));
+    }
+
+    protected virtual void ConfigureInterceptors(Configuration.Configuration configuration)
+    {
+    }
+
+    protected virtual void ConfigureParameterExtractors(Configuration.Configuration configuration)
+    {
+      configuration.ParameterExtractors.Add(new SpecifiedParameterParameterExtractor());
+      configuration.ParameterExtractors.Add(new DataTableParameterExtractor());
+      configuration.ParameterExtractors.Add(new DictionaryParameterExtractor(new SqlPrimitiveDataTypes()));
+      configuration.ParameterExtractors.Add(new EnumerableParameterExtractor(new SqlPrimitiveDataTypes()));
+      configuration.ParameterExtractors.Add(new ObjectPropertyParameterExtractor(new SqlPrimitiveDataTypes()));
+    }
+  }
+}
--- AdoExecutor/Core/QueryFactory/SqlQueryFactory.cs
+++ AdoExecutor/Core/QueryFactory/SqlQueryFactory.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using AdoExecutor.Core.Configuration.Infrastructure;
 using AdoExecutor.Core.ConnectionString;
 using AdoExecutor.Core.DataObjectFactory;
-using AdoExecutor.Core.ObjectBuilder;
-using AdoExecutor.Core.ParameterExtractor;
-using AdoExecutor.Core.Query.Infrastructure;
-using AdoExecutor.Core.QueryFactory.Infrastructure;
-using AdoExecutor.Utilities.Adapter.DataTable;
-using AdoExecutor.Utilities.Adapter.List;
-using AdoExecutor.Utilities.ObjectConverter;
-using AdoExecutor.Utilities.PrimitiveTypes;
 
 namespace AdoExecutor.Core.QueryFactory
 {
-  public class SqlQueryFactory : IQueryFactory
+  public class SqlQueryFactory : QueryFactoryBase
   {
     private readonly string _connectionStringAppConfigKey;
 
@@ -25,55 +16,14 @@ namespace AdoExecutor.Core.QueryFactory
       _connectionStringAppConfigKey = connectionStringAppConfigKey;
     }
 
-    public virtual IQuery CreateQuery()
-    {
-      IConfiguration configuration = CreateConfiguration();
-      return new Query.Query(configuration);
-    }
-
-    protected virtual IConfiguration CreateConfiguration()
-    {
-      var configuration = new Configuration.Configuration();
-      
-      ConfigureConnectionStringProvider(configuration);
-      ConfigureDataObjectFactory(configuration);
-      ConfigureObjectBuilders(configuration);
-      ConfigureInterceptors(configuration);
-      ConfigureParameterExtractors(configuration);
-
-      return configuration;
-    }
-
-    protected virtual void ConfigureConnectionStringProvider(Configuration.Configuration configuration)
+    protected override void ConfigureConnectionStringProvider(Configuration.Configuration configuration)
     {
       configuration.ConnectionStringProvider = new AppConfigConnectionStringProvider(_connectionStringAppConfigKey);
     }
 
-    protected virtual void ConfigureDataObjectFactory(Configuration.Configuration configuration)
+    protected override void ConfigureDataObjectFactory(Configuration.Configuration configuration)
     {
       configuration.DataObjectFactory = new SqlDataObjectFactory();
-    }
-
-    protected virtual void ConfigureObjectBuilders(Configuration.Configuration configuration)
-    {
-      configuration.ObjectBuilders.Add(new DataSetObjectBuilder(new DataTableAdapter()));
-      configuration.ObjectBuilders.Add(new DataTableObjectBuilder(new DataTableAdapter()));
-      configuration.ObjectBuilders.Add(new SimpleTypeObjectBuilder(new SqlPrimitiveDataTypes(), new ListAdapterFactory(), new ObjectConverter()));
-      configuration.ObjectBuilders.Add(new DefinedTypeObjectBuilder(new ListAdapterFactory(), new SqlPrimitiveDataTypes(), new ObjectConverter()));
-      configuration.ObjectBuilders.Add(new DynamicObjectBuilder(new ListAdapterFactory()));
-    }
-
-    protected virtual void ConfigureInterceptors(Configuration.Configuration configuration)
-    {
-    }
-
-    protected virtual void ConfigureParameterExtractors(Configuration.Configuration configuration)
-    {
-      configuration.ParameterExtractors.Add(new SpecifiedParameterParameterExtractor());
-      configuration.ParameterExtractors.Add(new DataTableParameterExtractor());
-      configuration.ParameterExtractors.Add(new DictionaryParameterExtractor(new SqlPrimitiveDataTypes()));
-      configuration.ParameterExtractors.Add(new EnumerableParameterExtractor(new SqlPrimitiveDataTypes()));
-      configuration.ParameterExtractors.Add(new ObjectPropertyParameterExtractor(new SqlPrimitiveDataTypes()));
     }
   }
 }
