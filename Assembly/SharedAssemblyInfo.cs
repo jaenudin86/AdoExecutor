962e330cb5bd37f45bf9d2b0adec664247a14835
Tomasz Reformat <t.reformat@hotmail.com>
2015-10-04 09:51:23 +0200

    Change release version

--- Assembly/SharedAssemblyInfo.cs
+++ Assembly/SharedAssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.11")]
-[assembly: AssemblyFileVersion("1.0.11")]
-[assembly: AssemblyInformationalVersion("1.0.11")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 
 #if !SNK
 [assembly: InternalsVisibleTo("AdoExecutor.UnitTest")]
